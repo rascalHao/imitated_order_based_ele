8d15314557ed6752b3d9a8835c4150198c384963
zhanghao <zhanghao@emicnet.com>
2017-02-12 17:44:41 +0800

    sticky

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,8 +17,28 @@
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
+			<div v-if="seller.supports" class="support-count" @click="showDetail">
+				<span class="count">{{seller.supports.length}}个</span>
+				<i class="icon-keyboard_arrow_right"></i>
+			</div>
 		</div>
-		<div class="bulletin-wrapper"></div>
+		<div class="bulletin-wrapper" @click="showDetail">
+			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+			<i class="icon-keyboard_arrow_right"></i>
+		</div>
+		<div class="background">
+			<img :src="seller.avatar" width="100%" height="100%">
+		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper clearfix">
+				<div class="detail-main">
+					<h1 class="name">{{seller.name}}</h1>
+				</div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close"></i>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -32,6 +52,16 @@
 		props: ['seller'],
 		created() {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+		},
+		data() {
+			return {
+				detailShow: false
+			}
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true
+			}
 		}
 	}
 </script>
@@ -39,9 +69,12 @@
 	@import('../../common/stylus/mixin.styl')
 
 	.header
+		position relative
 		color #fff
-		background #000
+		background rgba(7,17,27,0.5)
+		overflow hidden
 		.content-wrapper
+			position relative
 			padding 24px 12px 18px 24px
 			font-size 0
 			.avatar
@@ -94,4 +127,81 @@
 					.text
 						line-height 12px
 						font-size 10px
+			.support-count
+				position absolute
+				right 12px
+				bottom 18px
+				padding 0 8px
+				height 24px
+				line-height 24px
+				border-radius 14px
+				background rgba(0,0,0,0.2)
+				text-align center
+				.count
+					font-size  10px
+				.icon-keyboard_arrow_right
+					line-height 24px
+					margin-left 2px
+					font-size  10px
+		.bulletin-wrapper
+			position relative
+			height 28px
+			line-height 28px
+			padding 0 22px 0 12px
+			white-space nowrap
+			overflow hidden
+			text-overflow ellipsis
+			background rgba(7,17,27,0.2)
+			.bulletin-title
+				display inline-block
+				width 22px
+				height 12px
+				bg-image('bulletin')
+				background-size 22px 12px
+				background-repeat no-repeat
+			.bulletin-text
+				vertical-align top
+				margin 0 4px
+				font-size 10px
+			.icon-keyboard_arrow_right
+				position absolute
+				font-size 10px
+				right 12px
+				top 8px
+		.background
+			position absolute
+			top 0
+			left 0
+			width 100%
+			height 100%
+			z-index -1
+			filter blur(10px)
+		.detail
+			position fixed
+			top 0
+			left 0
+			z-index 100
+			width 100%
+			height 100%
+			overflow auto
+			background rgba(7,17,27,0.8)
+			.detail-wrapper
+				width 100%
+				min-height 100%
+				.detail-main
+					margin-top 64px
+					padding 0 36px 64px 36px
+					.name
+						line-height 16px
+						text-align center
+						font-size 16px
+						font-weight 700
+			.detail-close
+				position relative
+				width 32px
+				height 32px
+				margin -64px auto 0 auto
+				clear both
+				font-size 32px
+
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="star" :class="starType">
+		<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['size', 'number'],
+		computed: {
+			starType() {
+				return 'star-' + this.size
+			}
+		}
+	}
+</script>
+
+<style lang="stylus">
+	.star
+		font-size 0
+		.star-item
+			display inline-block
+			background no-repeat
+			&.star-48
+			&.star-36
+			&.star-24
+</style>
